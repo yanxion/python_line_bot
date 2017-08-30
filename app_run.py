7a2972a451d676758e65684b9b0560233f82f750
yanxion <yanxionmouse@gmail.com>
2017-08-30 15:11:08 +0800

    Config Variables done.

---
+++ app_run.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ.get("Channel_Access_Token", None))
+handler = WebhookHandler(os.environ.get("Channel_Secret", None))
+
+@app.route("/")
+def index():
+    html_script = "<h1> Heroku app success !! </h1>"
+    return html_script
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
